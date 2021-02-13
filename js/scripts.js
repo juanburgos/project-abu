3d1fba453c13dcf5a4b6199145ace3cf65274202
JuanBurgos <JuanBurgos@outlook.com>
2021-02-12 23:15:40 -0500

    Add recipe modal and refractor code

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ $(function () {
         $("#signInModal").modal();
     });
 });
+
+$(function () {
+    $(".recipe-demo").click(function () {
+        $("#recipeModal").modal();
+    });
+});
+
+$(function () {
+    $("#bookmarkRecipe").click(function () {
+        $("#bookmarkRecipe").toggleClass('active');
+    });
+});
